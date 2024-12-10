f4cff74692371d93a577db0635de1757ca83c6c8
Tapabrata Chowdhury <tapabratachowdhury12@gmail.com>
2024-12-10 10:25:13 +0530

    Added my Flask Learning

---
+++ Flask/_3_Jinja_Templates_Inheritance/parameter_app.py
@@ -0,0 +1,17 @@
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/home')
+
+def home():
+    return render_template("p_home.html",title = "Home")
+
+@app.route('/about')
+
+def about():
+    return render_template("p_about.html",title = "About")
+
+if __name__ == "__main__":
+    app.run(debug=True)
